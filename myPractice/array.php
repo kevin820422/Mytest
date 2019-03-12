08a11eb274e805a5abf88c6a826606f100df6abb
kevin820422 <kevin820422@gmail.com>
2019-03-12 17:14:17 +0800

    Update array.php

--- myPractice/array.php
+++ myPractice/array.php
@@ -1,32 +1,13 @@
 <?php 
-$myArray=[];
-$myArray[]=1;
-$myArray[]=3;
-foreach ($myArray as $k){
-    echo $k;
-};
-print_r($myArray); 
-$food=array('french'=>['entree','plates','dessert'],'chinese'=>['appetizer','main_dishes','soup','fruit','tea']);
+$myA=[1,2,3,4,5];
+$myA2=[4,2,5,6,7];
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php foreach($food as $k): ?>
-    <?php $n=count($food[$k]); ?>
-    <?= "{$k} style cuisine mainly have {$n} dishes."  ?>
-    <?php endforeach ?>
-        
-   
-    
-</body>
-</html>
+$myA3=array_diff($myA2,$myA);
+
+print_r($myA3);
+
+
+
 
 
     
